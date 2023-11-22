1e12fcaac7059ac69701fe0b91cb7b8d7d9a8e3d
jm-marqueti <149432125+jm-marqueti@users.noreply.github.com>
2023-11-22 10:39:14 -0300

    Changed regs to wires

--- dct_transform_1d/toplevel.v
+++ dct_transform_1d/toplevel.v
@@ -1,13 +1,14 @@
 module toplevel(input signed [0:63]X, 
 output signed[0:143]Y);
 
-reg signed [0:7]v[0:7]; // v contém os elementos de X separados em um array
-integer i;
-always @* begin
+wire signed [0:7]v[0:7]; // v contém os elementos de X separados em um array
+genvar i;
+
+generate
   for (i = 0; i < 8; i = i + 1) begin
-	v[i] = X[8*i +: 8];
+	assign v[i] = X[8*i +: 8];
   end
-end
+endgenerate
 
 //even-odd decomp (depth0)
 wire signed [0:8] E[0:3], O[0:3];
@@ -26,7 +27,7 @@ wire signed [15:0] x18[0:3],x50[0:3],x75[0:3],x89[0:3];
 wire signed [0:17] Ye[0:3],  Yo[0:3];
 
 	//EVEN
-	
+
 generate // SAU Setup
 	for (j = 0; j < 4; j = j + 1) begin: evenSAUloop
 		SAUe evenSAU(E[j],x36[j],x64[j],x83[j]);
@@ -40,7 +41,7 @@ assign Ye[2] = x64[0] -x64[1] -x64[2] +x64[3];
 assign Ye[3] = x36[0] -x83[1] +x83[2] -x36[3];
 
 	//ODD
-	
+
 generate // SAU Setup
 	for (j = 0; j < 4; j = j + 1) begin: oddSAUloop
 		SAUo oddSAU(O[j],x18[j],x50[j],x75[j],x89[j]);
@@ -55,14 +56,14 @@ assign Yo[3] = x18[0] -x50[1] +x75[2] -x89[3];
 
 
 //rearranjo para o resultado final
-reg signed [0:17] Mix [0:7];
+wire signed [0:17] Mix [0:7];
 
-always @* begin
+generate
   for (i = 0; i < 4; i = i + 1) begin
-	Mix[2*i] = Ye[i];
-	Mix[2*i + 1] = Yo[i];
+	assign Mix[2*i] = Ye[i];
+	assign Mix[2*i + 1] = Yo[i];
   end
-end
+endgenerate
 
 assign Y = {Mix[0],Mix[1],Mix[2],Mix[3],Mix[4],Mix[5],Mix[6],Mix[7]};
 
