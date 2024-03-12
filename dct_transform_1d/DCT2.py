321078a42058b5d78ee6c3579e38b59f0e1019b3
Gustavo Russi <78427180+Russi1555@users.noreply.github.com>
2024-03-12 19:12:22 -0300

    Update DCT2.py

--- dct_transform_1d/DCT2.py
+++ dct_transform_1d/DCT2.py
@@ -3,37 +3,29 @@ import math
 from math import sqrt, cos
 import math
 import random
-###  python3 DCT2.py > out.txt  ###
+###  python3 DCT2.py > goldenmodel.dat  ###
 
 
-def decimal_to_18bit_binary(decimal_number):
-    # Ensure the number is within the range of an 18-bit signed integer
-    if decimal_number < -262144 or decimal_number > 262143:
-        raise ValueError("Decimal number is out of the 18-bit signed integer range")
+def decimal_to_16bit_binary(decimal_number):
+    if decimal_number < -32768 or decimal_number > 32767:
+        raise ValueError("Decimal number is out of the 16-bit signed integer range")
 
-    # Check if the number is negative
     if decimal_number < 0:
-        # Calculate the two's complement representation
-        binary_number = bin(decimal_number & 0x3FFFF)[2:]
+        binary_number = bin(decimal_number & 0xFFFF)[2:]
     else:
-        # Convert the positive number to binary with leading zeros
-        binary_number = format(decimal_number, '018b')
+        binary_number = format(decimal_number, '016b')
 
     return binary_number
 
 
-def decimal_to_8bit_signed_binary(decimal_number):
-    # Ensure the number is within the range of an 8-bit signed integer (-128 to 127)
-    if decimal_number < -128 or decimal_number > 127:
-        raise ValueError("Decimal number is out of the 8-bit signed integer range")
+def decimal_to_32bit_binary(decimal_number):
+    if decimal_number < -2147483648  or decimal_number > 2147483647:
+        raise ValueError("Decimal number is out of the 32-bit signed integer range")
 
-    # Check if the number is negative
     if decimal_number < 0:
-        # Calculate the two's complement representation
-        binary_number = bin(decimal_number & 0xFF)[2:]
+        binary_number = bin(decimal_number & 0xFFFFFFFF)[2:]
     else:
-        # Convert the positive number to binary with leading zeros
-        binary_number = format(decimal_number, '08b')
+        binary_number = format(decimal_number, '032b')
 
     return binary_number
 
@@ -45,74 +37,52 @@ def DCT_2(N):
             alpha = 1 if i == 0 else math.sqrt(2)
             t = alpha * math.cos((math.pi * (2 * j + 1) * i) / (2 * N))
             t = round(t * 64)
-            if t==84:
-                t=83
-            elif t==-84:
-                t=-83
-
-            elif t==35:
-                t=36
-            elif t ==-35:
-                t=-36
-
-            elif t==26:
-                t=25
-            elif t==-26:
-                t=-25
-
-            elif t==30:
-                t=31
-            elif t==-30:
-                t=-31
-
-            elif t==39:
-                t=38
-            elif t==-39:
-                t=-38
-
-            elif t==47:
-                t=46
-            elif t==-47:
-                t=-46
+            arredondamentos = {84: 83, -84: -83, 35: 36, -35: -36, 26: 25, -26: -25, 30: 31, -30: -31, 39: 38, -39: -38, 47: 46, -47: -46}
+            if t in arredondamentos:
+                t = arredondamentos[t]
+            else:
+                t = t
             linha.append(t)
         kernel.append(linha)
     return kernel
 
 if __name__ == "__main__":
-    N = 32
-    lim_inf = -128
-    lim_sup = 127
-    result = DCT_2(N)
-    for linha in result:
-        print(linha)
+    prefixos_de_tamanho = {4: "00", 8: "01", 16: "10", 32: "11"}
+    for cases in range(0,1000):
+        i = random.randint(0,3)
+        N = 4 * pow(2,i)
 
-    for c in range(0,1000):
-        x = []#[random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup),random.randint(lim_inf,lim_sup)]
+        lim_inf = -32768
+        lim_sup = 32767
+        kernel = DCT_2(N)
+
+        x = []
         x_bin = []
-        for i in range(0,N):
+        for l in range(0,N):
             temp = []
-            for j in range(0,N):
+            for m in range(0,int(32/N)):
                 temp.append(random.randint(lim_inf,lim_sup))
             x.append(temp)
-            x_bin.append(list(map(decimal_to_8bit_signed_binary,temp)))
-
-        transformed = np.matmul(result,x)
-        #print(transformed)
-
- 
-       # x_bin = list(map(decimal_to_8bit_signed_binary,x))
-
+            x_bin.append(list(map(decimal_to_16bit_binary,temp)))
+        transformed = np.matmul(kernel,x)
 
         x_input = ""
         for linha in x_bin:
             for vetor in linha:
                 x_input+=str(vetor)
+        formatacao_input = prefixos_de_tamanho[N]
+        for j in range(0,512-(len(x_input))):
+            formatacao_input += '0'
+        formatacao_input+=x_input
 
         output = ""
+        formatacao_output = prefixos_de_tamanho[N]
         for linha in transformed:
             for vetor in linha:
-                #print(vetor)
-                output+=str(decimal_to_18bit_binary(vetor))
+                output+=str(decimal_to_16bit_binary(int(vetor/pow(2,11))))
 
-        print(x_input + " " + output)
+        for j in range(0,512-(len(output))):
+            formatacao_output += '0'
 
+        formatacao_output+=output
+        print(formatacao_input + " " + formatacao_output)
