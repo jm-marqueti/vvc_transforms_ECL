fd8810d039c8bd93d333d29285799d7d2fa8a11c
jm-marqueti <149432125+jm-marqueti@users.noreply.github.com>
2024-06-11 21:49:04 -0300

    add plk_to_csv

---
+++ modelo_laplace/plk_to_csv.py
@@ -0,0 +1,11 @@
+import pickle as pkl
+import pandas as pd
+import sys
+
+#python3 plk_to_csv.py <input> <output>
+
+with open(str(sys.argv[1]), "rb") as f:
+    object = pkl.load(f)
+
+df = pd.DataFrame(object,index=[0])
+df.to_csv(str(sys.argv[2]))
