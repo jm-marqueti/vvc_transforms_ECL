461fc812b6c65b7a9ea7cc63d60f88498bce4ca8
jm-marqueti <149432125+jm-marqueti@users.noreply.github.com>
2024-06-19 13:50:52 -0300

    Update toplevel.sv

--- dct2_1d/toplevel.sv
+++ dct2_1d/toplevel.sv
@@ -1,5 +1,5 @@
 module toplevel(input signed [0:511]X_test,
-input [1:0]N_in,
+input [1:0],
 input clk,
 output reg [0:511]Y);
 
@@ -8,44 +8,40 @@ wire signed [17:0] X_8[0:7], DCT_8_input [0:7], Y8E [0:7];
 wire signed [18:0] X_4[0:3], DCT_4_input[0:3], Y4E[0:3];
 wire signed [26:0] Y2E[0:1], Y2O[0:1], Y4O[0:3], Y8O[0:7], Y16O[0:15];
 wire signed [15:0] Y2E_shifted[0:1], Y2O_shifted[0:1], Y4O_shifted[0:3], Y8O_shifted[0:7], Y16O_shifted[0:15];
-reg signed [15:0] X[0:31];
-reg [1:0]N;
+reg signed [15:0]X[0:31];
 
-always @(posedge clk) begin
- N <= N_in;
- X[0] <= X_test[0:15];
- X[1] <= X_test[16:31];
- X[2] <= X_test[32:47];
- X[3] <= X_test[48:63];
- X[4] <= X_test[64:79];
- X[5] <= X_test[80:95];
- X[6] <= X_test[96:111];
- X[7] <= X_test[112:127];
- X[8] <= X_test[128:143];
- X[9] <= X_test[144:159];
- X[10]<= X_test[160:175];
- X[11]<= X_test[176:191];
- X[12]<= X_test[192:207];
- X[13]<= X_test[208:223];
- X[14]<= X_test[224:239];
- X[15]<= X_test[240:255];
- X[16]<= X_test[256:271];
- X[17]<= X_test[272:287];
- X[18]<= X_test[288:303];
- X[19]<= X_test[304:319];
- X[20]<= X_test[320:335];
- X[21]<= X_test[336:351];
- X[22]<= X_test[352:367];
- X[23]<= X_test[368:383];
- X[24]<= X_test[384:399];
- X[25]<= X_test[400:415];
- X[26]<= X_test[416:431];
- X[27]<= X_test[432:447];
- X[28]<= X_test[448:463];
- X[29]<= X_test[464:479];
- X[30]<= X_test[480:495];
- X[31]<= X_test[496:511];
-end
+assign X[0] = X_test[0:8];
+assign X[1] = X_test[9:17];
+assign X[2] = X_test[18:26];
+assign X[3] = X_test[27:35];
+assign X[4] = X_test[36:44];
+assign X[5] = X_test[45:53];
+assign X[6] = X_test[54:62];
+assign X[7] = X_test[63:71];
+assign X[8] = X_test[72:80];
+assign X[9] = X_test[81:89];
+assign X[10] = X_test[90:98];
+assign X[11] = X_test[99:107];
+assign X[12] = X_test[108:116];
+assign X[13] = X_test[117:125];
+assign X[14] = X_test[126:134];
+assign X[15] = X_test[135:143];
+assign X[16] = X_test[144:152];
+assign X[17] = X_test[153:161];
+assign X[18] = X_test[162:170];
+assign X[19] = X_test[171:179];
+assign X[20] = X_test[180:188];
+assign X[21] = X_test[189:197];
+assign X[22] = X_test[198:206];
+assign X[23] = X_test[207:215];
+assign X[24] = X_test[216:224];
+assign X[25] = X_test[225:233];
+assign X[26] = X_test[234:242];
+assign X[27] = X_test[243:251];
+assign X[28] = X_test[252:260];
+assign X[29] = X_test[261:269];
+assign X[30] = X_test[270:278];
+assign X[31] = X_test[279:287];
 
 // signed bit extension on each element
 assign  X_16[0] = {X[0][15] , X[0]};
@@ -125,7 +121,7 @@ assign Y16O_shifted[13]= Y16O[13][26:11];
 assign Y16O_shifted[14]= Y16O[14][26:11];
 assign Y16O_shifted[15]= Y16O[15][26:11];
 
-permutate PERMUTATE(Y2E_shifted, Y2O_shifted, Y4O_shifted, Y8O_shifted, Y16O_shifted, N,clk, Y); 
+permutate PERMUTATE(Y2E_shifted, Y2O_shifted, Y4O_shifted, Y8O_shifted, Y16O_shifted, N, Y); 
 
 
 endmodule
